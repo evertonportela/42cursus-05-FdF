4f99d82c4c16a4be9faed9b44d67783eb4f50793
evertonportela <evertonportela@outlook.com>
2023-08-22 10:33:08 -0300

    update: format message error mandatory

--- mandatory/sources/utils/utils_check_args.c
+++ mandatory/sources/utils/utils_check_args.c
@@ -6,7 +6,7 @@
 /*   By: evportel <evportel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 11:46:14 by evportel          #+#    #+#             */
-/*   Updated: 2023/08/16 12:00:15 by evportel         ###   ########.fr       */
+/*   Updated: 2023/08/22 10:29:04 by evportel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 int	utils_error_message(void)
 {
 	write(1, "Use ./fractol [Fractal Name]\n", 30);
-	write(1, "(1) Mandelbrot\n", 16);
+	write(1, "(1) Mandelbrot\n\n", 17);
 	write(1, "(2) Julia [real param] [imaginary param]\n", 42);
 	write(1, "Suggestions:\n", 14);
 	write(1, "-0.4  0.6 |  0.285 0   |  0.45 0.1428\n", 39);
-	write(1, "-0.6 -0.2 | -0.54  0.5 | -0.08 0.72\n", 37);
+	write(1, "-0.6 -0.2 | -0.54  0.5 | -0.08 0.72\n\n", 38);
 	write(1, "Keyboard Shotcuts:\n", 20);
 	write(1, "Q - Iterations Out\t'-' Zoom Out\n", 33);
 	write(1, "E - Iteration In\t'+' Zoom In\n", 30);
